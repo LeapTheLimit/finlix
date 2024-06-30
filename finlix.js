9a0d7b763fc6e53ffedd3eaccaf4b52fe2a93192
ABEDQWASMI <165468797+ABEDQWASMI@users.noreply.github.com>
2024-06-30 15:53:34 +0300

    Update finlix.js

--- finlix.js
+++ finlix.js
@@ -427,13 +427,3 @@
 
     window.initializeAssistantWidget = initWidget;
 })();
-
-window.onload = function() {
-    // Load Axios library dynamically
-    const script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js';
-    script.onload = function() {
-        initializeAssistantWidget();
-    };
-    document.head.appendChild(script);
-};
